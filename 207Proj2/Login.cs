7673885895ea7c2e8ec292bcd7a95cce0c4f4e42
Mitchell Dundas Smith <803100@ACDM.DS.SAIT.CA>
2019-01-07 18:30:01 -0700

    Few Tweaks to Login Page
    Some stylizing done
    Stille more to do
    Cannot get the login page to pop up before the mainform

--- 207Proj2/Login.cs
+++ 207Proj2/Login.cs
@@ -17,6 +17,6 @@ namespace _207Proj2
             InitializeComponent();
         }
 
-        
+       
     }
 }
--- 207Proj2/Login.designer.cs
+++ 207Proj2/Login.designer.cs
@@ -28,17 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.pnlLeft = new System.Windows.Forms.Panel();
             this.lblApp = new System.Windows.Forms.Label();
             this.lblTravelExperts = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pnlRight = new System.Windows.Forms.Panel();
+            this.lblLoginPage = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.lblPassword = new System.Windows.Forms.Label();
             this.lblUsername = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.pnlLeft.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.pnlRight.SuspendLayout();
@@ -53,7 +56,7 @@
             this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlLeft.Location = new System.Drawing.Point(0, 0);
             this.pnlLeft.Name = "pnlLeft";
-            this.pnlLeft.Size = new System.Drawing.Size(291, 438);
+            this.pnlLeft.Size = new System.Drawing.Size(266, 438);
             this.pnlLeft.TabIndex = 0;
             // 
             // lblApp
@@ -61,7 +64,7 @@
             this.lblApp.AutoSize = true;
             this.lblApp.Font = new System.Drawing.Font("Lucida Sans Unicode", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblApp.ForeColor = System.Drawing.Color.White;
-            this.lblApp.Location = new System.Drawing.Point(66, 298);
+            this.lblApp.Location = new System.Drawing.Point(56, 302);
             this.lblApp.Name = "lblApp";
             this.lblApp.Size = new System.Drawing.Size(173, 70);
             this.lblApp.TabIndex = 2;
@@ -72,16 +75,16 @@
             this.lblTravelExperts.AutoSize = true;
             this.lblTravelExperts.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTravelExperts.ForeColor = System.Drawing.Color.White;
-            this.lblTravelExperts.Location = new System.Drawing.Point(12, 31);
+            this.lblTravelExperts.Location = new System.Drawing.Point(64, 9);
             this.lblTravelExperts.Name = "lblTravelExperts";
-            this.lblTravelExperts.Size = new System.Drawing.Size(276, 45);
+            this.lblTravelExperts.Size = new System.Drawing.Size(155, 90);
             this.lblTravelExperts.TabIndex = 1;
-            this.lblTravelExperts.Text = "Travel Experts";
+            this.lblTravelExperts.Text = " Travel\r\nExperts";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(72, 125);
+            this.pictureBox1.Location = new System.Drawing.Point(62, 111);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(157, 147);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -90,23 +93,35 @@
             // 
             // pnlRight
             // 
+            this.pnlRight.Controls.Add(this.lblLoginPage);
             this.pnlRight.Controls.Add(this.btnLogin);
             this.pnlRight.Controls.Add(this.txtPassword);
             this.pnlRight.Controls.Add(this.txtUsername);
             this.pnlRight.Controls.Add(this.lblPassword);
             this.pnlRight.Controls.Add(this.lblUsername);
             this.pnlRight.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlRight.Location = new System.Drawing.Point(291, 0);
+            this.pnlRight.Location = new System.Drawing.Point(266, 0);
             this.pnlRight.Name = "pnlRight";
-            this.pnlRight.Size = new System.Drawing.Size(357, 438);
+            this.pnlRight.Size = new System.Drawing.Size(290, 438);
             this.pnlRight.TabIndex = 1;
+            // 
+            // lblLoginPage
+            // 
+            this.lblLoginPage.AutoSize = true;
+            this.lblLoginPage.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginPage.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblLoginPage.Location = new System.Drawing.Point(42, 35);
+            this.lblLoginPage.Name = "lblLoginPage";
+            this.lblLoginPage.Size = new System.Drawing.Size(216, 45);
+            this.lblLoginPage.TabIndex = 3;
+            this.lblLoginPage.Text = "Login Page";
             // 
             // btnLogin
             // 
             this.btnLogin.BackColor = System.Drawing.Color.MidnightBlue;
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(139, 354);
+            this.btnLogin.Location = new System.Drawing.Point(105, 358);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(96, 42);
             this.btnLogin.TabIndex = 7;
@@ -116,17 +131,22 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(84, 288);
+            this.txtPassword.Location = new System.Drawing.Point(50, 302);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '•';
             this.txtPassword.Size = new System.Drawing.Size(198, 20);
             this.txtPassword.TabIndex = 6;
+            this.txtPassword.Text = "Enter Password";
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(84, 139);
+            this.txtUsername.Location = new System.Drawing.Point(50, 183);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(198, 20);
             this.txtUsername.TabIndex = 5;
+            this.txtUsername.Text = "Enter Username";
+            this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lblPassword
             // 
@@ -134,7 +154,7 @@
             this.lblPassword.BackColor = System.Drawing.SystemColors.Control;
             this.lblPassword.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPassword.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.lblPassword.Location = new System.Drawing.Point(119, 215);
+            this.lblPassword.Location = new System.Drawing.Point(92, 251);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(116, 25);
             this.lblPassword.TabIndex = 4;
@@ -146,17 +166,22 @@
             this.lblUsername.BackColor = System.Drawing.SystemColors.Control;
             this.lblUsername.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUsername.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.lblUsername.Location = new System.Drawing.Point(119, 85);
+            this.lblUsername.Location = new System.Drawing.Point(85, 129);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(123, 25);
             this.lblUsername.TabIndex = 3;
             this.lblUsername.Text = "Username:";
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(648, 438);
+            this.ClientSize = new System.Drawing.Size(556, 438);
             this.Controls.Add(this.pnlRight);
             this.Controls.Add(this.pnlLeft);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -174,7 +199,6 @@
         #endregion
 
         private System.Windows.Forms.Panel pnlLeft;
-        private System.Windows.Forms.Label lblTravelExperts;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel pnlRight;
         private System.Windows.Forms.Label lblApp;
@@ -183,5 +207,8 @@
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblTravelExperts;
+        private System.Windows.Forms.Label lblLoginPage;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
